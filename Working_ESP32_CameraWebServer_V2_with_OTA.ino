6dd692acd773486044d65b5c75e9da3a5ae9d465
wjsanek <86143642+wjsanek@users.noreply.github.com>
2021-07-14 07:45:48 -0400

    Add files via upload

---
+++ Working_ESP32_CameraWebServer_V2_with_OTA.ino
@@ -0,0 +1,58 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <ESPmDNS.h> //Required to compile on Russian 'ArduinoDroid' IDE but not required on Arduino IDE
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+/*
+ * BOARD SETTINGS
+ * 
+ * Board type: ESP32 Wrover Module
+ * 
+ */
+
+// Getting WIFI_SSID and WIFI_PASSWORD from external credentials file
+//#include "jtektiot_credentials.h";
+const char* WIFI_SSID = "HAL9000";
+const char* WIFI_PASSWORD = "Znildorkpad5+";
+
+// Wifi settings
+#define HOSTNAME "camera"
+#define WIFI_CONNECTION_TIMEOUT 5000
+
+// Camera pinout
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_init();
+  wifi_setup();
+  startCameraServer();
+  ota_setup();
+}
+
+void loop() {
+  wifi_connection_manager();
+  ArduinoOTA.handle();
+}
